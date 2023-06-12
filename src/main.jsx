3c2244ac606abbf7dac9b1a9dfa2ed64caaa3cf8
Martin <jmartinaguirre79@gmail.com>
2023-06-12 10:52:08 -0300

    Cambio de nombre

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// Donde se hace el root se pone el archivo de sas
+import './styles/styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
